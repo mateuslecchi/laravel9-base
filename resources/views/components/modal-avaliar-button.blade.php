afbfa5d7854c8b0c876bc0f75f5c7c3c1a1d5434
Mateus Lecchi <mateus.lecchi.y@gmail.com>
2022-12-26 22:00:45 -0300

    removiing old assets

--- resources/views/components/modal-avaliar-button.blade.php
+++
@@ -1,8 +0,0 @@
-@props([
-'target' => 'target'
-])
-
-<button type="button" data-bs-toggle="modal" data-bs-target="#{{ $target }}" class="btn btn-sm bg-warning ms-1 ">
-    <i class="bi bi-star-fill"></i>
-        Avaliar
-</button>
--- resources/views/components/modal-devolver-button.blade.php
+++
@@ -1,8 +0,0 @@
-@props([
-'target' => 'target'
-])
-
-<button type="button" data-bs-toggle="modal" data-bs-target="{{ $target }}" class="btn btn-sm bg-warning ">
-    <i class="bi bi-arrow-return-left"></i>
-    Devolver
-</button>
